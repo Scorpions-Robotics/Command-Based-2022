64290a8ba50edd7fe3a7b838488815999212bf63
Ardasak <ardasak434@gmail.com>
2022-02-21 22:40:30 -0800

    İrfanıııım

--- src/main/java/frc/robot/commandgroups/Telenomous/Telenomous2Balls.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commandgroups.Telenomous;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-public class Telenomous2Balls extends SequentialCommandGroup {
-  public Telenomous2Balls() {
-    addCommands();
-  }
-}
