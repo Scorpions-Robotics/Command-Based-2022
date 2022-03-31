3a50fc413d0641c60e1cb4e55702d20fa7c0f79e
Arda Sak <ardasak434@gmail.com>
2022-04-01 00:42:39 +0300

    ci test

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,7 +21,6 @@ public final class Constants {
     public static final int kButton12 = 12;
     public static final int kButton13 = 13;
   }
-
   public static class FEEDER {
     public static final int limitPort = 0;
     public static final int distanceSensorPort = 1;
